f7ab1bccc5bbd1054a19cfe996809f6c2be7e0d7
JUNE <63999666+DevelopJun@users.noreply.github.com>
2021-07-28 17:46:34 +0900

    Add files via upload

--- CI2/Models/util/Display_model.php
+++ CI2/Models/util/Display_model.php
@@ -26,196 +26,58 @@ class Display_model
 	}
 
 	public function init_Navi(){
+		$oKn = new DB('ks_navigation', 'ksadmin');
 
-		// $this->aTop['data'] = ['testing'=>'now'];
-		// $this->aLeft['data'] = ['testing'=>'next'];
+		// 3중 for문 이외의 query 조회시 정렬하여 받아올 수 있도록 table 수정
+		$oKn->Where("kn_display_flag = 'Y'");
+		$oKn->Where("kn_depth = '0'");
+		// print_r($oKn);
+		$aRoot = $oKn->GetData();
+		$this->aTop['data'] = [];
+		print_r($aRoot);
+
+		foreach ($aRoot as $nKey1 => $nRoot){
+			$this->aTop['data'][$nKey1] = [
+				'name' => $nRoot['kn_name'],
+				'url' => $nRoot['kn_url'],
+				'child' => []
+			];
+
+			$oKn->Where("kn_display_flag = 'Y'");
+			$oKn->Where("kn_depth = '1'");
+			$oKn->Where("kn_first_idx = '".$nRoot['kn_idx']."'");
+			$aFirst = $oKn->GetData();
+			foreach ($aFirst as $nKey2 => $nFirst){
+				$this->aTop['data'][$nKey1]['child'][$nKey2] = [
+					'name' => $nFirst['kn_name'],
+					'url' => $nFirst['kn_url'],
+					'child' => []
+				];
+				$oKn->Where("kn_display_flag = 'Y'");
+				$oKn->Where("kn_depth = '2'");
+				$oKn->Where("kn_second_idx = '".$nFirst['kn_idx']."'");
+				$aSecond = $oKn->GetData();
+				foreach ($aSecond as $nKey3 => $nSecond){
+					$this->aTop['data'][$nKey1]['child'][$nKey2]['child'][$nKey3] = [
+						'name' => $nSecond['kn_name'],
+						'url' => $nSecond['kn_url']
+					];
+				}
+			}
+		}
+		/*
+		echo "<pre>";
+		print_r($this->aTop['data']);
+		echo "</pre>";
+		*/
 
 		$oKn = new DB('ks_navigation', 'ksadmin');
-		$aData = $oKn->GetData();
-		// $oKn->where('kn_name', $aData['kn_name']);
-		// print_r(gettype(count($aData)));
-		// print_r($aData[0]);
-
-		$i = 0;
-		$aReturnTop = [];
-		$aReturnSide = [];
-
-		while($i < count($aData)){
-			$valuesArray = array_values($aData[$i]);
-			// array_push($aReturnVriable, [
-			// 	'kn_name'=> $valuesArray[1],
-			// 	'kn_url'=> $valuesArray[2],
-			// 	'kn_depth'=> $valuesArray[3],
-			// 	'kn_depth1'=> $valuesArray[4],
-			// ]);
-			if($valuesArray[3] == 0 ){
-				array_push($aReturnTop, $valuesArray[1]);
-			}else{
-				array_push($aReturnSide, $aData[$i]);
-			}
-			$i ++;
+		$oKn->Where("kn_display_flag = 'Y'");
+		$oKn->Where("kn_url = '".$_SERVER['REQUEST_URI']."'");
+		$aLocation = $oKn->GetDataOne();
+		if ($aLocation){
+			// 해당하는 location의 상위, 동위, 하위 조회(order by로 정렬하여)
 		}
-
-		$valuesArray1 = array_values($aReturnTop);
-		print_r($valuesArray1[0]);
-		print_r($valuesArray1[1]);
-
-
-		$this->aTop['data'] = ['test1'=>$valuesArray1[0], 'test2' => $valuesArray1[1]];
-		// $this->aTop['data'] = ['testing'=>$valuesArray1[1]];
-		//
-		// print_r($aReturnTop);
-		// echo "<br>";
-		// print_r($aReturnSide);
-
-
-		return;
-
-
-		// print_r($aReturnVriable);
-
-
-		//
-		// $valuesArray1 = array_values($aData[0]);
-		//
-		//
-		// $valuesArray2 = array_values($aData[1]);
-		// $valuesArray3 = array_values($aData[2]);
-
-
-		// json 추출 실패 -> json 추출시 string으로 되기 때문에 key, value 뽑기 힘듬//
-/*
-		$jData =  json_encode($aData); // array json 변환
-		print_r($jData);
-		print_r(gettype($jData));
-		print_r($jData[0]['kn_name']);
-		foreach ($jData[0]['data'] as $field => $value) {
- }
-		json_encode($aData) // array json 변환
-		print_r($aData);
-		print_r(gettype($aData));*/
-		//
-		// $Data1 = [
-		// 	'kn_name'=> $valuesArray1[1],
-		// 	'kn_url'=> $valuesArray1[2],
-		// 	'kn_depth'=> $valuesArray1[3],
-		// 	'kn_depth1'=> $valuesArray1[4],
-		// ];
-		//
-		//
-		// $Data2 = [
-		// 	'kn_name'	=> $valuesArray2[1],
-		// 	'kn_url'	=> $valuesArray2[2],
-		// 	'kn_depth'=> $valuesArray2[3],
-		// 	'kn_depth1'=> $valuesArray1[4],
-		// ];
-		// $Data3 = [
-		// 	'kn_name'	=> $valuesArray3[1],
-		// 	'kn_url'	=> $valuesArray3[2],
-		// 	'kn_depth'=> $valuesArray3[3],
-		// 	'kn_depth1'=> $valuesArray1[4],
-		// ];
-
-
-		// return $this->parse($sContent, $aData, true);
-		//
-		//
-		// $parser->setData($data)
-		//
-		// if(데이터 관련 관련 조건문 삽입){
-		// 	return $this->display->load('/index', $kData, true);
-		// }
-		// else{
-		// 	return $this->display->load('/index', $lData, true);
-		// }
-
-
-		// $this->aTop['data'] = [];
-		// $NO = 0;
-		// $_topmenu_html = "";
-		// foreach($_topmenu_arr as $key => $val)
-		// {
-		// 	if($NO==0) $_topmenu_html .= "<li style='margin-left:1px'>";
-		// 	else $_topmenu_html .= "<li>";
-		//
-		// 	if(in_array($val[0]['kn_url'],$_topmenu_url_arr) && $val[0]['kn_url']!="" && $val[0]['kn_url']!="/"){ // 권한이 있으면 그대로 보여준다.
-		// 		$kn_url_A = $val[0]['kn_url'];
-		// 	}else{ // 권한이 없으면 첫번째 소분류 메뉴로 링크
-		// 		$kn_url_A = $_submenu_arr[$val[0]['kn_first_code']][0]['kn_url'];
-		// 	}
-		//
-		// 	$_topmenu_html .= "<a href='".$kn_url_A."' onmouseover='change_bg(".$change_bg_arr[$NO].");' >".$val[0]['kn_group_name']."</a>";
-		//
-		// 	$_topmenu_html .= "<ul class='first_dept' style='width:600px'>";
-		// 		foreach($val as $val2)
-		// 		{
-		// 			$_topmenu_html .= "<li>";
-		//
-		// 			if(in_array($val2['kn_url'],$_topmenu_url_arr) && $val2['kn_url']!="" && $val2['kn_url']!="/"){
-		// 				$kn_url_B = $val2['kn_url'];
-		// 			}else{
-		// 				$kn_url_B = $_submenu_arr[$val2['kn_first_code']][0]['kn_url'];
-		// 			}
-		//
-		// 			$_topmenu_html .= "<a href='".$kn_url_B."'>".$val2['kn_first_name']."</a>";
-		//
-		//
-		// 			if(is_array($_submenu_arr[$val2['kn_first_code']])){
-		// 				$_topmenu_html .= "<ul class='dropdown-menu' style='z-index:1000; position:absolute; width:260px !important; padding:10px;'>";
-		// 				foreach($_submenu_arr[$val2['kn_first_code']] as $val3){
-		// 					$_topmenu_html .= "<li><a href='".$val3['kn_url']."'>".$val3['kn_second_name']."</a></li>";
-		// 				}
-		// 				$_topmenu_html .= "</ul>";
-		// 			}
-		// 			$_topmenu_html .= "</li>";
-		// 		}
-		// 	$_topmenu_html .= "</ul>";
-		// 	$_topmenu_html .= "</li>";
-		// 	$NO++;
-		// }
-		//
-		//
-		// $this->aLeft['data'] = [];
-		// $_leftmenu_html = "";
-		// if($result_A)
-		// {
-		// 	if($result_A['kn_depth']=="1"){
-		// 		$kn_group = $result_A['kn_group'];
-		// 	} else if($result_A['kn_depth']=="2") {
-		// 		// 같은 소분류 구하기.
-		// 		$sql_B = "SELECT * FROM ks_navigation_new WHERE kn_disp_flag='Y' AND kn_first_code = '".$result_A['kn_first_code']."' AND kn_depth='1' ORDER BY kn_ord_no ASC LIMIT 0,1";
-		// 		$result_B = $this->oDb->FetchOne($sql_B);
-		// 		$kn_group = $result_B['kn_group'];
-		// 	}
-		//
-		// 	$_leftmenu_html .= "<div class='subtop'>";
-		// 		$_leftmenu_html .= "<ul>";
-		// 			$_leftmenu_html .= "<li style='font-size:11pt;'>".$_topmenu_arr[$kn_group][0]['kn_group_name']."</li>";
-		// 			foreach($_topmenu_arr[$kn_group] as $key => $val)
-		// 			{
-		// 				if(in_array($val['kn_url'],$_topmenu_url_arr) && $val['kn_url']!="" && $val['kn_url']!="/") $kn_url_B = $val['kn_url'];
-		// 				else $kn_url_B = $_submenu_arr[$val['kn_first_code']][0]['kn_url'];
-		//
-		// 				if($result_A['kn_first_code']==$val['kn_first_code'])
-		// 				{
-		// 					if(is_array($_submenu_arr[$val['kn_first_code']]))
-		// 					{
-		// 						$_leftmenu_html .= "<li class='listm_on'><span style='font-weight:bold;'>".$val['kn_first_name']."</span></li>";
-		//
-		// 						 foreach($_submenu_arr[$val['kn_first_code']] as $val2)
-		// 						{
-		// 							if($result_A['kn_idx']==$val2['kn_idx']) $_leftmenu_html .= "<li class='listm_l'><a href='".$val2['kn_url']."' class='left_subtitle'><span style='font-weight:bold;color:red;'>".$val2['kn_second_name']."</span></a></li>";
-		// 							else $_leftmenu_html .= "<li class='listm_l'><a href='".$val2['kn_url']."' class='left_subtitle'>".$val2['kn_second_name']."</a></li>";
-		// 						}
-		// 					}
-		// 					else  $_leftmenu_html .= "<li class='listm_on'><a href='".$kn_url_B."'><span style='font-weight:bold;color:red;'>".$val['kn_first_name']."</span></a></li>";
-		// 				}
-		// 				else $_leftmenu_html .= "<li class='listm'><a href='".$kn_url_B."'><span style='font-weight:bold;'>".$val['kn_first_name']."</span></a></li>";
-		// 			}
-		// 		$_leftmenu_html .= "</ul>";
-		// 	$_leftmenu_html .= "</div>";
-		// }
-
 	}
 
 	public function setResponse($response) {
@@ -271,22 +133,12 @@ class Display_model
 	//디스플레이 출력
 	public function load($sContent, $aData = array(), $bParser = false)
 	{
-		// $parser = \Config\Services::parser();
 		$this->init_Navi();
-
-		print_r($this->aTop["data"]);
-		// $this->aTop["data"];
-		// print_r($this->aLeft["data"]);
-		// $this->aLeft['data'] = $aData1;
-		// $this->parser->render();
-		// echo $parser->setData($finaldata)->render('top.php');
-
 		$aLayout = $this->aLayout["data"]; //Layout 기본 데이터
 		$this->aHeader["data"]["TITLE"] = "testing";
 		$aLayout["header"] = $this->parse($this->aHeader["form"], $this->aHeader["data"], true);
-		$aLayout["top"] = $this->parse($this->aTop["form"], $this->aTop["data"], true);
-
-		$aLayout["left"] = $this->parse($this->aLeft["form"], $this->aLeft["data"], true);
+		$aLayout["top"] = view($this->aTop["form"], $this->aTop['data']);
+		$aLayout["left"] = view($this->aLeft["form"], $this->aLeft['data']);
 
 		if ($bParser) { //내용을 파서로 처리하는 경우
 			$aLayout["content"] = $this->parse($sContent, $aData, true);
