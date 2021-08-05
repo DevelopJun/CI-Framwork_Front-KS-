d51723271b778d5b1f3811d9965b977514910097
JUNE <63999666+DevelopJun@users.noreply.github.com>
2021-08-05 17:53:04 +0900

    Add files via upload

--- CI2/public/js/Ajax/Ajax(common).js
+++ CI2/public/js/Ajax/Ajax(common).js
@@ -3,15 +3,23 @@
 * --------------------
 *author 정준호
 */
+// 재 발급 받는 부분
+function errorCallback(){
+	alert("콜백 하잉~");
+	return 0;
+}
+
+
 
 var AJAX = {
-	// form => form 내용, oken => accesstoken 값
-	post: function(form, token){
+	// get 구현 X
+	// urlc=>, form => form 내용, oken => accesstoken 값
+	post: function(urlc, form, token){
 		$.ajax({
 			type: "post",
 			data: form.serialize(),
 			contentType: "application/x-www-form-urlencoded",
-			url: "https://apigw.ksdev.net/api/v1/ksadmin/admin/board",
+			url: urlc, //url로 하면 이름 같아서 에러 발생
 			beforeSend:function(xhr){
 				xhr.setRequestHeader('Authorization', 'Bearer '+ token);
 			},
@@ -19,16 +27,24 @@ var AJAX = {
 				if (data.code == 200){
 					alert("입력완료");
 					location.href = "/company/board/detail?idx="+data.data.idx;
-					console.log(data);
+					console.log(data.message);
 				} else {
 					alert(data.message);
 				}
 			},
 			fail: function (data){
-				alert(data);
+				if(data.code == 401){
+					errorCallback();
+				}else{
+					alert("콜백함수로 안갔어. 10초 안에 들어왔어");
+				}
 			},
 			error: function (data){
-				alert(data);
+				if(data.code == 401){
+					errorCallback();
+				}else{
+					alert("콜백함수로 안갔어. 10초 안에 들어왔어");
+				}
 			}
 		});
 	},
@@ -60,14 +76,14 @@ var AJAX = {
 		});
 
 	},
-	// idx => 게시물 번호, Aidx => 사용자 번호, idxn=> url idx 번호, token => accesstoken 값
-	delete: function(idx, Aidx, idxn, token){
+	// url => url 로, idx => 게시물 번호, Aidx => 사용자 번호, idxn=> url idx 번호, token => accesstoken 값
+	delete: function(url, idx, Aidx, idxn, token){
 		$.ajax({
 			type: "delete",
 			async:false,
 			data: {idx:idx,admin_idx:Aidx},
 			contentType: "application/x-www-form-urlencoded",
-			url: `https://apigw.ksdev.net/api/v1/ksadmin/admin/board/${idxn}`,
+			url: url + idxn,
 			beforeSend:function(xhr){
 				xhr.setRequestHeader('Authorization', 'Bearer '+ token);
 			},
---
+++ CI2/public/js/Ajax/setcookie.js
@@ -0,0 +1,32 @@
+/*
+*accesstoken cookie 저장 구간 (보안 우회)
+*--------------------
+*author 정준호
+*/
+
+// 쿠키에서 토큰 받아 오는 구간
+function getCookie(key){
+    // var cookie = document.cookie;
+    // if(){
+    //
+    // }
+    var result = document.cookie.split("=");
+
+    // 그럼 여기서 accesstoken을 재발급 받야 하는가?
+    if(key === result[0]){
+        return result[1];
+    }else{
+        return "key 틀렸습니다. 값 다시 입력하세요."
+    };
+}
+
+
+// token header 맞추는 구간
+function header(token){
+    var headerfile = [
+    'Authorization: Bearer '+ token,
+    'Content-Type: application/json'
+    ];
+    headerfileC = JSON.stringify(headerfile); // object -> json 변환
+    return headerfileC;
+}
