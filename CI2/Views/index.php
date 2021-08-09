c8c2bf982d4bcd688d7fb052d363f23b520c8d2a
JUNE <63999666+DevelopJun@users.noreply.github.com>
2021-08-09 17:55:08 +0900

    Add files via upload

--- CI2/Views/index.php
+++ CI2/Views/index.php
@@ -1,11 +1,35 @@
-<script>
-	console.log("testing");
-</script>
-<h2>메인 페이지</h2>
-<div class="dombox_tt">
-	<h2>{ ka_id }</h2>
-	<h2>{ ka_name }</h2>
-	<h2>{ ka_email }</h2>
-	<h2>{ ka_join_date }</h2>
-	<a href="#" onclick="location.href='/login/logout';"><i class="fas fa-chevron-circle-right"></i> 로그아웃</a>
+<div class="app-page-title">
+	<div class="page-title-wrapper">
+		<div class="page-title-heading">
+			<div>
+				메인 페이지
+				<div class="page-title-subheading">
+					살짝의 설명이 들어갈 수 있는 부분
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-12 col-lg-12">
+		<div class="mb-3 card">
+			<div class="card-header-tab card-header-tab-animation card-header">
+				<div class="card-header-title">
+					<i class="header-icon lnr-apartment icon-gradient bg-love-kiss"></i>
+					메인 디자인 헤더 부분 생략 가능
+				</div>
+			</div>
+			<div class="card-body">
+				<div class="tab-content">
+					<div class="tab-pane fade show active" id="tabs-eg-77" style="min-height: 400px">
+						<h2>{ ka_id }</h2>
+						<h2>{ ka_name }</h2>
+						<h2>{ ka_email }</h2>
+						<h2>{ ka_join_date }</h2>
+						<a href="#" onclick="location.href='/login/logout';"><i class="fas fa-chevron-circle-right"></i> 로그아웃</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
--- CI2/Views/menu1.php
+++ CI2/Views/menu1.php
@@ -1,11 +1,25 @@
+
 <script>
 	console.log("testing");
 </script>
-<h2>메인 페이지</h2>
+<h2>테스팅 페이지</h2>
 <div class="dombox_tt">
-	<h2>{ ka_id }</h2>
-	<h2>{ ka_name }</h2>
-	<h2>{ ka_email }</h2>
-	<h2>{ ka_join_date }</h2>
-	<a href="#" onclick="location.href='/login/logout';"><i class="fas fa-chevron-circle-right"></i> 로그아웃</a>
+<h2>
+	<?php
+	// 여기만 테스트 페이지로 남겨 놓겠습니다.
+	$arr = array(
+	        "kshosting" => array("june" => "intern1", "Hyewon" => "intern2"),
+	        "number" => array("one" => 1, "two" => 2, "three" => 3),
+	        "animal" => array("zibra", "bear")
+	        );
+	        foreach( $arr as $key => $value) {
+	            echo "첫번째 배열 key=".$key.", 이중배열 : ";
+	            foreach($value as $key2 => $value2) {
+	                echo "key=".$key2.", ";
+	                echo "value=".$value2.", ";
+	            }
+	            echo "<br>";
+	        }
+	 ?>
+</h2>
 </div>
